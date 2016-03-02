c636f0a9dfd23211ca410bc31a1e8d5a8cbc9ec8
Angad Dubey <angad_dubey@Hotmail.com>
2016-03-02 15:45:29 -0500

    update title

--- resources/views/templates/main.blade.php
+++ resources/views/templates/main.blade.php
@@ -5,7 +5,7 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1">
 
-    <title>Icebreker - Know more about your lead</title>
+    <title>Icebreaker - Know more about your lead</title>
     <link href="//fonts.googleapis.com/css?family=Lato:100,300,400,700" rel="stylesheet" type="text/css">
     <link rel="stylesheet" type="text/css" href="//cdnjs.cloudflare.com/ajax/libs/font-awesome/4.4.0/css/font-awesome.min.css">
     <link rel="stylesheet" type="text/css" href="//maxcdn.bootstrapcdn.com/bootstrap/3.3.6/css/bootstrap.min.css">
